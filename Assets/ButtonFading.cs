e8820e954b7996a282478bad1bb41857f2413d9f
Pavel Bu <73419843+puchik006@users.noreply.github.com>
2023-08-03 14:15:24 +0300

    Graphics extension

--- Assets/ButtonFading.cs
+++ Assets/ButtonFading.cs
@@ -1,66 +1,17 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonFading : MonoBehaviour
 {
-    [SerializeField] private Image imageToFadeOut;
-    [SerializeField] private Image imageToFadeIn;
-    [SerializeField] private float fadeDuration = 1.0f;
-
-    private void Start()
-    {
-        // Ensure the first image is visible, and the second image is not
-        imageToFadeOut.gameObject.SetActive(true);
-        imageToFadeIn.gameObject.SetActive(false);
-    }
+    [SerializeField] private Graphic _handImage;
+    [SerializeField] private Graphic _handText;
+    [SerializeField] private Graphic _map; 
+    [SerializeField] private float _fadeDuration = 1.0f;
 
     public void OnButtonPressed()
     {
-        StartCoroutine(FadeOutAndIn());
-    }
-
-    private IEnumerator FadeOutAndIn()
-    {
-        yield return FadeOut();
-        yield return FadeIn();
-    }
-
-    private IEnumerator FadeOut()
-    {
-        float elapsedTime = 0f;
-        Color startColor = imageToFadeOut.color;
-        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
-
-        while (elapsedTime < fadeDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / fadeDuration);
-            imageToFadeOut.color = Color.Lerp(startColor, endColor, t);
-            yield return null;
-        }
-
-        // Deactivate the first image after fading out
-        imageToFadeOut.gameObject.SetActive(false);
-    }
-
-    private IEnumerator FadeIn()
-    {
-        imageToFadeIn.gameObject.SetActive(true);
-        float elapsedTime = 0f;
-        Color startColor = new Color(1f, 1f, 1f, 0f);
-        Color endColor = new Color(1f, 1f, 1f, 1f);
-
-        while (elapsedTime < fadeDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            float t = Mathf.Clamp01(elapsedTime / fadeDuration);
-            imageToFadeIn.color = Color.Lerp(startColor, endColor, t);
-            yield return null;
-        }
-
-        // Ensure the second image's alpha is fully set to 1 after fading in
-        imageToFadeIn.color = new Color(endColor.r, endColor.g, endColor.b, 1f);
+        StartCoroutine(_handImage.FadeOut(_fadeDuration));
+        StartCoroutine(_handText.FadeIn(_fadeDuration));
+        StartCoroutine(_map.FadeOut(_fadeDuration));
     }
 }
-
---
+++ Assets/GraphicExtensions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public static class GraphicExtensions
+{
+    public static IEnumerator FadeOut(this Graphic graphicToFadeOut, float fadeDuration)
+    {
+        float elapsedTime = 0f;
+        Color startColor = graphicToFadeOut.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / fadeDuration);
+            graphicToFadeOut.color = Color.Lerp(startColor, endColor, t);
+            yield return null;
+        }
+
+        graphicToFadeOut.gameObject.SetActive(false);
+    }
+
+    public static IEnumerator FadeIn(this Graphic graphicToFadeIn, float fadeDuration)
+    {
+        graphicToFadeIn.gameObject.SetActive(true);
+        float elapsedTime = 0f;
+        Color startColor = new Color(1f, 1f, 1f, 0f);
+        Color endColor = new Color(1f, 1f, 1f, 1f);
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / fadeDuration);
+            graphicToFadeIn.color = Color.Lerp(startColor, endColor, t);
+            yield return null;
+        }
+
+        graphicToFadeIn.color = new Color(endColor.r, endColor.g, endColor.b, 1f);
+    }
+}
