fb4b171cf536f84b06f580795ccc738f96f5cf37
Pavel Bu <puchik006@mail.ru>
2023-08-04 15:52:37 +0300

    Zooming Image

---
+++ Assets/ZoomUtility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class ZoomUtility
+{
+    public static void ZoomIn(this Image image, float zoomStep, float maxZoom)
+    {
+        Vector3 scale = image.transform.localScale;
+        scale *= 1 + zoomStep;
+        scale = Vector3.Min(scale, new Vector3(maxZoom, maxZoom, 1.0f));
+        image.transform.localScale = scale;
+    }
+
+    public static void ZoomOut(this Image image, float zoomStep, float minZoom)
+    {
+        Vector3 scale = image.transform.localScale;
+        scale /= 1 + zoomStep;
+        scale = Vector3.Max(scale, new Vector3(minZoom, minZoom, 1.0f));
+        image.transform.localScale = scale;
+    }
+
+    public static void ResetZoom(this Image image, float originalScale)
+    {
+        image.transform.localScale = new Vector3(originalScale, originalScale, 1.0f);
+    }
+}
+
---
+++ Assets/ZoomingImage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ZoomingImage : MonoBehaviour
+{
+    private Image _imageToZoom;
+    [SerializeField] private Button _zoomInButton;
+    [SerializeField] private Button _zoomOutButton;
+    [SerializeField] private Button _resetZoomButton;
+
+    private float _originalScale;
+    private float _zoomStep = 0.1f;
+    private float _maxZoom = 1.5f; 
+    private float _minZoom = 0.5f;
+
+    private void Awake()
+    {
+        _imageToZoom = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        _originalScale = _imageToZoom.transform.localScale.x;
+
+        _zoomInButton.onClick.AddListener(() => _imageToZoom.ZoomIn(_zoomStep,_maxZoom));
+        _zoomOutButton.onClick.AddListener(() => _imageToZoom.ZoomOut(_zoomStep, _minZoom));
+        _resetZoomButton.onClick.AddListener(() => _imageToZoom.ResetZoom(_originalScale));
+    }
+}
+
